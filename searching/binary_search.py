634fbe29b3d3178185a75dd070d6559b4a317741
jesse collier <jesse.collier97@gmail.com>
2019-04-13 12:46:00 +1200

    added iterative binary search --neither tested

--- searching/binary_search.py
+++ searching/binary_search.py
@@ -11,3 +11,33 @@ def binary_search_recursive(arr, value, low, high):
     return binary_search_recursive(arr, value, mid + 1, high)
   else:
     return mid
+
+  #     BinarySearch(A[0..N-1], value) {
+  #     low = 0
+  #     high = N - 1
+  #     while (low <= high) {
+  #         // invariants: value > A[i] for all i < low
+  #                        value < A[i] for all i > high
+  #         mid = (low + high) / 2
+  #         if (A[mid] > value)
+  #             high = mid - 1
+  #         else if (A[mid] < value)
+  #             low = mid + 1
+  #         else
+  #             return mid
+  #     }
+  #     return not_found // value would be inserted at index "low"
+  # }
+
+  def binary_Search(arr, value):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+      mid = math.floor((low + high)/2)
+      if arr[mid] > value:
+        high = mid - 1
+      elif arr[mid] < value:
+        low = mid + 1
+      else:
+        return mid
+    return None
